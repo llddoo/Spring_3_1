399db503d7085507700aee395d5b2ce8ee6f5380
이석준 <llddoo@naver.com>
2021-03-23 14:50:41 +0900

    ㅇㅇ1

--- src/test/java/com/iu/s3/member/MemberDAOTest.java
+++ src/test/java/com/iu/s3/member/MemberDAOTest.java
@@ -4,7 +4,24 @@ package com.iu.s3.member;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
-public class MemberDAOTest extends MyAbstractTest{
+import com.iu.s3.MyAbstractTest;
+
+public class MemberDAOTest extends MyAbstractTest {
+	
+	@Autowired
+	private MemberDAO memberDAO;
+	
+	@Test
+	public void memberJoin() throws Exception {
+		
+		MemberDTO memberDTO = new MemberDTO();
+		
+		
+		
+		assertEquals(1, result);
+		
+	}
 	
 }
